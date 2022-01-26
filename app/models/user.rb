3ed49c858880dccaa07336e45bac154969c1b090
Jhanvi <83320700+JhanviShah1@users.noreply.github.com>
2022-01-26 13:05:59 -0500

    Capybara and Postgeist setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, confirmation: true, length: {minimum: 6}
+  def self.authenticate_with_credentials(email, password)
+    @user = self.where("lower(email) = ?", email.delete(' ').downcase).first.try(:authenticate, password)
+  end
 end
